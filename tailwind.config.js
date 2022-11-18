bea0159b0b9076fff3827c29107382927f0fa2d9
fakhryfernanda <mff023@gmail.com>
2022-11-18 07:53:10 +0700

    Finishing mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      container: {
+        screens: {
+          xl: '1170px'
+        },
+      },
       colors: {
         main_red: '#FF401B',
+        main_red_darker: 'rgb(242,120,103)',
         secondary_red: '#FF846C',
         main_orange: '#FFAA04',
         main_grey: '#7B7B7B'
